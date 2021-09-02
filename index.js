eca50df514d18a2d7edf9fa0159b976f72fb5a0b
Sea Of Dreams <filippo.livorno@gmail.com>
2021-09-02 15:38:33 +0200

    add skeleton

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 fetch('https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=20')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+        console.log(data)
+        document.getElementById("asset-image").innerHTML = `
+        <img class="asset-img" src="${data.assets[0].image_url}" />
+        `
+        document.getElementById("asset-title").textContent = data.assets[0].name
+
+    })
